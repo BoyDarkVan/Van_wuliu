fcb6c9afcda9c6ee769f7bf4082b025c161b805b
AwenMr <1490969163@qq.com>
2019-04-29 11:22:18 +0800

    项目重构

--- van_mapper/src/main/java/com/van/mapper/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.van.mapper;
-
-import com.van.moudle.Testpojo;
-
-public interface TestDao {
-
-    Testpojo login(Testpojo admin);
-
-}
--- van_service/src/main/java/com/van/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.van.service;
-
-import com.van.moudle.Testpojo;
-
-public interface TestService {
-
-    Testpojo login(Testpojo admin);
-}
