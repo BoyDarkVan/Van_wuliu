ce0a4c9259049dfcfdc0c2f4c4d904b2e69f799a
wellhey01 <532003845@qq.com>
2019-04-30 09:53:01 +0800

    改变方法返回值

--- van_mapper/src/main/java/com/van/mapper/AdminMapper.java
+++ van_mapper/src/main/java/com/van/mapper/AdminMapper.java
@@ -3,9 +3,12 @@ package com.van.mapper;
 import com.van.pojo.Admin;
 
 public interface AdminMapper {
+
     Admin loginAdmin(Admin admin);
 
-    int updAdm(String adm_id,String adm_pwd);
+    void updAdm(String adm_id,String adm_pwd);
 
-    int addAdm(String adm_id,String adm_name,String adm_pwd);
+    void addAdm(String adm_id,String adm_name,String adm_pwd);
+
+
 }
