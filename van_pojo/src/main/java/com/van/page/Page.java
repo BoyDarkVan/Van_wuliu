5ac7810723e01c70b4d44a8e44e8d6ddb1276772
AwenMr <1490969163@qq.com>
2019-05-04 23:20:32 +0800

    page封装信息

---
+++ van_pojo/src/main/java/com/van/page/Page.java
@@ -0,0 +1,130 @@
+package com.van.page;
+
+import java.io.Serializable;
+
+/**
+ * 处理分页
+ */
+public class Page implements Serializable {
+
+    //当前页
+    private Integer page = 1;
+    //页大小
+    private Integer rows = 5;
+    // 总记录 数
+    private Integer totalRecord;
+    //总页数
+    private Integer totalPage;
+    //关键字类型
+    private String keyType;
+    //查询关键字
+    private String keyWord;
+    //开始记录位置
+    private Integer start;
+    //用户id
+    private String userId;
+    //其他用户id
+    private String otherId;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getTotalRecord() {
+        return totalRecord;
+    }
+
+    public void setTotalRecord(Integer totalRecord) {
+        this.totalRecord = totalRecord;
+    }
+
+    public Integer getTotalPage() {
+
+        totalPage = (totalRecord - 1) / rows + 1;
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public String getKeyType() {
+        return keyType;
+    }
+
+    public void setKeyType(String keyType) {
+        this.keyType = keyType;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    public Integer getStart() {
+        start = (page - 1) * rows;
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOtherId() {
+        return otherId;
+    }
+
+    public void setOtherId(String otherId) {
+        this.otherId = otherId;
+    }
+
+    public Page(Integer page, Integer rows, Integer totalRecord, Integer totalPage, String keyType, String keyWord, Integer start, String userId, String otherId) {
+        this.page = page;
+        this.rows = rows;
+        this.totalRecord = totalRecord;
+        this.totalPage = totalPage;
+        this.keyType = keyType;
+        this.keyWord = keyWord;
+        this.start = start;
+        this.userId = userId;
+        this.otherId = otherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Page{" +
+                "page=" + page +
+                ", rows=" + rows +
+                ", totalRecord=" + totalRecord +
+                ", totalPage=" + totalPage +
+                ", keyType='" + keyType + '\'' +
+                ", keyWord='" + keyWord + '\'' +
+                ", start=" + start +
+                ", userId='" + userId + '\'' +
+                ", otherId='" + otherId + '\'' +
+                '}';
+    }
+}
