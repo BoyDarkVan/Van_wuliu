b2ddcb4b53215835d7cd0c3ce7f9b690788e28ff
AwenMr <1490969163@qq.com>
2019-05-04 23:20:01 +0800

    UsersService

---
+++ van_service/src/main/java/com/van/service/UsersService.java
@@ -0,0 +1,23 @@
+package com.van.service;
+
+import com.van.pojo.Users;
+
+import com.van.page.Page;
+
+import java.util.List;
+
+public interface UsersService {
+
+//  查询所有用户
+    List<Users> findAllUsers();
+
+//    分页数据
+    List<Users> selectPageList(Page page);
+
+//    分页总数
+
+    Integer selectPageCount(Page page);
+
+//    根据用户id 删除用户
+    void delUsersById(String userId);
+}
