7590a656d80c7b86173878e1829f80ee1aac4090
wellhey01 <532003845@qq.com>
2019-05-05 10:48:32 +0800

    layui分类，staff表

--- van_mapper/src/main/java/com/van/mapper/StaffMapper.java
+++ van_mapper/src/main/java/com/van/mapper/StaffMapper.java
@@ -1,10 +1,14 @@
 package com.van.mapper;
 
+import com.van.page.Page;
 import com.van.pojo.Staff;
 
 import java.util.List;
 
 public interface StaffMapper {
-    List<Staff> findAllStaff();
+
+    List<Staff> findAllStaff(Page page);
+
+    Integer findPagetotal(Page page);
 
 }
